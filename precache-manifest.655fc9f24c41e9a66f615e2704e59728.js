1adfdae3795884ec6d76767944c545963eb83165
Kamil Żogło <kamilzoglo@gmail.com>
2020-01-27 21:00:11 +0100

    Updates

---
+++ precache-manifest.655fc9f24c41e9a66f615e2704e59728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4827a86cb8c933f18da4a3142d24694",
+    "url": "/CodersCamp_MiniKino_Frontend/index.html"
+  },
+  {
+    "revision": "0be708d5f3be65da7dfc",
+    "url": "/CodersCamp_MiniKino_Frontend/static/css/main.42b98a40.chunk.css"
+  },
+  {
+    "revision": "ab3383e61978aed9ef56",
+    "url": "/CodersCamp_MiniKino_Frontend/static/js/2.0fd04c55.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/CodersCamp_MiniKino_Frontend/static/js/2.0fd04c55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0be708d5f3be65da7dfc",
+    "url": "/CodersCamp_MiniKino_Frontend/static/js/main.80f60757.chunk.js"
+  },
+  {
+    "revision": "4c07b780543bf1cb1370",
+    "url": "/CodersCamp_MiniKino_Frontend/static/js/runtime-main.73f6f7d3.js"
+  }
+]);
