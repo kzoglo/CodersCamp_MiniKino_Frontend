146d00406dd3efb22bf9c60957e2074a980ccad1
Kamil <kamilzoglo@gmail.com>
2020-09-15 14:14:58 +0200

    Updates

--- precache-manifest.f60225621b2abc1cf1e9a5acedfa4b48.js
+++ precache-manifest.f60225621b2abc1cf1e9a5acedfa4b48.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7a0abdfe06b318e1a29ca8f9efb74a1",
+    "revision": "dcc76abe56c862f0df7f2e67afb4ab2e",
     "url": "/CodersCamp_MiniKino_Frontend/index.html"
   },
   {
-    "revision": "f166f9064b8f27038211",
-    "url": "/CodersCamp_MiniKino_Frontend/static/css/main.95d26ce8.chunk.css"
+    "revision": "df3577fb3f68629f594e",
+    "url": "/CodersCamp_MiniKino_Frontend/static/css/main.f5d21b3b.chunk.css"
   },
   {
     "revision": "34ef1e6ca7fa9aed79aa",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/CodersCamp_MiniKino_Frontend/static/js/2.3ae13fcb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f166f9064b8f27038211",
-    "url": "/CodersCamp_MiniKino_Frontend/static/js/main.8ee54fb2.chunk.js"
+    "revision": "df3577fb3f68629f594e",
+    "url": "/CodersCamp_MiniKino_Frontend/static/js/main.aacd97bf.chunk.js"
   },
   {
     "revision": "6a68664ddd08111ae810",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CodersCamp_MiniKino_Frontend/precache-manifest.dbc8502a9cfb7635f622721018568af5.js"
+  "/CodersCamp_MiniKino_Frontend/precache-manifest.f60225621b2abc1cf1e9a5acedfa4b48.js"
 );
 
 self.addEventListener('message', (event) => {
