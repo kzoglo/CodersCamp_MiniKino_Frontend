3c819ce76e24e8cb773b3eea6f1e1e665a52fe94
Kamil Żogło <zoglokamil@gmail.com>
2021-09-08 13:16:36 +0200

    TS transition: - project-wide enums created

---
+++ enums/index.ts
@@ -0,0 +1,13 @@
+export enum ClassesToManipulate {
+  INVISIBLE = 'invisible',
+  VISIBLE = 'visible',
+  SHOW = 'show',
+  HIDE = 'hide',
+  GRID = 'grid',
+  CURSOR_POINTER = 'cursor-pointer',
+  SUBMIT_BTN = 'submitBtn'
+}
+
+export enum TimerValues {
+  TIME_TO_SHOW_REMINDER = 300000
+}
